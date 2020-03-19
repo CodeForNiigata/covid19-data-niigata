9f32fee37a9c35f447549d902c257b7429b57bdf
Matsui Masashi <matsui@kaffeine.jp>
2020-03-19 21:31:41 +0900

    職業も改行文字を変換するようにした

--- src/main.py
+++ src/main.py
@@ -23,6 +23,7 @@ houkoku_table.columns = ['no', 'case_no', 'date', 'age', 'sex', 'address', 'job'
 houkoku_table['date'] = '2020年' + houkoku_table['date']
 houkoku_table['date'] = pd.to_datetime(houkoku_table['date'], format='%Y年%m月%d日')
 houkoku_table['address'] = houkoku_table['address'].replace('\r', '', regex=True)
+houkoku_table['job'] = houkoku_table['job'].replace('\r', '', regex=True)
 houkoku_table.to_csv('houkoku.csv')
 
 soudan_pdf = tabula.read_pdf(base_url + soudan_url, pages='all')
