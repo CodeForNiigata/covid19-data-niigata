0d1734158deb57dc8729cf74d9d66e360b6c9b1a
Matsui Masashi <matsui@kaffeine.jp>
2020-03-20 11:37:21 +0900

    main.pyから引数で色々動作するようにした

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-from scraper import get_url
 from inspections import create_csv as create_inspections_csv
 from patients import create_csv as create_patients_csv
 from querents import create_csv as create_querents_csv
+from scraper import get_url
+from gs import convert
+import sys
 
-patients_url, querents_url, inspections_url = get_url()
-create_patients_csv(patients_url)
-create_inspections_csv(inspections_url)
-create_querents_csv(querents_url)
+
+def main():
+    args = sys.argv
+    if args[1] == 'scrape':
+        patients_url, querents_url, inspections_url = get_url()
+        if args[2] in {'inspections', 'all'}:
+            create_inspections_csv(inspections_url)
+        if args[2] in {'patients', 'all'}:
+            create_patients_csv(patients_url)
+        if args[2] in {'querents', 'all'}:
+            create_querents_csv(querents_url)
+        if not args[2] in {'all', 'inspections', 'patients', 'querents'}:
+            print('invalid second argument !')
+    elif args[1] == 'convert':
+        convert()
+    else:
+        print('invalid first argument !')
+
+
+if __name__ == "__main__":
+    main()
